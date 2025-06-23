5a057dbcbd3bff99a019360734ad741b595e420d
Jakub Gladysz <jakub.gladysz@protonmail.com>
2025-06-23 11:59:16 +0300

    use le bytes fn

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,13 @@ pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
         return Err("Transaction data too short".into());
     }
 
-    let version_bytes = transaction_bytes[..4]
-        .to_vec()
-        .iter()
-        .map(|x| x.to_owned() as u32)
-        .collect::<Vec<u32>>();
-    let first_byte = version_bytes
-        .first()
-        .expect("First byte not found")
-        .to_owned();
+    let version_bytes = &transaction_bytes[..4];
+    // either TryFrom or TryInto
+    // let version_bytes: Result<[u8; 4], TryFromSliceError> = <[u8; 4]>::try_from(version_bytes);
+    let version_bytes: [u8; 4] = version_bytes
+        .try_into()
+        .map_err(|_| "Invalid version bytes")?;
+    let le_bytes = u32::from_le_bytes(version_bytes);
 
-    Ok(first_byte)
+    Ok(le_bytes)
 }
